c597bb68039f3deb80fe8d862dcd2ccf25ed8696
twaller9 <111897068+twaller9@users.noreply.github.com>
2022-08-29 00:32:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Thomas Waller
+ * twaller9@huskers.unl.edu
+ * Date: 2022/08/28git 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hi, I'm Thomas Waller.\n");
+    printf("My major is Computer Science\n");
+
+    return 0;
+}
